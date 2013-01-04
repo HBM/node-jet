d62e543268bda4459198eed177af19b137971e30
Gerhard Lipp <gelipp@gmail.com>
2013-01-04 14:59:10 +0100

    switch to mocha test framework

--- test/message-socket-test.js
+++ test/message-socket-test.js
@@ -1,48 +1,89 @@
-var vows = require('vows');
-var assert = require('assert');
 var net = require('net');
+var should = require('should');
+var EventEmitter = require('events').EventEmitter;
 /* this is a private module, so load directly */
 var MessageSocket = require('../lib/jet/message-socket.js').MessageSocket;
 
 var echoPort = 1337;
-var testMessage = {
+var testMessageA = {
     a: 123,
     b: true
 };
+var testMessageB = 71117;
 
-var echoServer = net.createServer(function(socket) {
-    socket.pipe(socket);
+before(function(done) {
+    var echoServer = net.createServer(function(socket) {
+        socket.pipe(socket);
+    });
+    echoServer.listen(echoPort);
+    done();
 });
-echoServer.listen(echoPort);
 
-vows.describe('The Message Socket internal class').addBatch({
-    'A MessageSocket': {
-        topic: function() {
-            var socket = net.connect(echoPort);
-            return new MessageSocket(socket);
-        },
-        on: {
-            'connect': {
-                'results in a MessageSocket': function(ms) {
-                    assert.instanceOf(ms, MessageSocket);
-                },
-                'provides sendMessage': function(ms) {
-                    assert.isFunction(ms.sendMessage);
-                },
-                'when sending a message to the echo server': {
-                    topic: function(ms) {
-                        ms.sendMessage(testMessage);
-                        return ms;
-                    },
-                    on: {
-                        'message': {
-                            'results in echoed message': function(echoed) {
-                                assert.deepEqual(testMessage, echoed);
-                            }
-                        }
+describe('A MessageSocket(socket)', function() {
+    var ms;
+    before(function(done) {
+        var socket = net.connect(echoPort);
+        socket.on('connect', function() {
+            ms = new MessageSocket(socket);
+            done();
+        });
+    });
+    it('should provide the essential interface', function() {
+        ms.should.be.an.instanceof(MessageSocket);
+        ms.should.be.an.instanceof(EventEmitter);
+        ms.sendMessage.should.be.a('function');
+        ms.beginBatch.should.be.a('function');
+        ms.endBatch.should.be.a('function');
+    });
+    describe('sending to the echo server', function() {
+        describe('a single message', function() {
+            it('should emit "messages" array of length 1', function(done) {
+                ms.once('messages', function(messages) {
+                    messages.should.be.an.instanceof(Array);
+                    messages.should.have.length(1);
+                    should.deepEqual(messages[0], testMessageA);
+                    done();
+                });
+                ms.sendMessage(testMessageA);
+            });
+            it('should emit "sent" with the unmodified message', function(done) {
+                ms.once('sent', function(message) {
+                    should.deepEqual(message, testMessageA);
+                    done();
+                });
+                ms.sendMessage(testMessageA);
+            });
+        });
+        describe('two messages at once', function() {
+            var sendTwoMessagesAtOnce = function() {
+                ms.sendMessage(testMessageA);
+                ms.sendMessage(testMessageB);
+            };
+            it('should emit "messages" array of length 2', function(done) {
+                ms.once('messages', function(messages) {
+                    messages.should.be.an.instanceof(Array);
+                    messages.should.have.length(2);
+                    should.deepEqual(messages[0], testMessageA);
+                    should.deepEqual(messages[1], testMessageB);
+                    done();
+                });
+                sendTwoMessagesAtOnce();
+            });
+            it('"sent" should emit the unmodified message', function(done) {
+                var count = 0;
+                var checkSentMessages = function(message) {
+                    if (count === 0) {
+                        message.should.equal(testMessageA);
                     }
-                }
-            }
-        }
-    }
-}).export(module);
+                    else if (count == 1) {
+                        message.should.equal(testMessageB);
+                        ms.removeListener('_sent', checkSentMessages);
+                        done();
+                    }++count;
+                };
+                ms.on('sent', checkSentMessages);
+                sendTwoMessagesAtOnce();
+            });
+        });
+    });
+})
