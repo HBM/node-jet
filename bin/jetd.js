46635030e9eb99f778eada07a0c505e16d1ee9e0
Gerhard Lipp <gelipp@gmail.com>
2013-06-25 00:07:47 +0200

    add static file webserver for radar

--- bin/jetd.js
+++ bin/jetd.js
@@ -1,6 +1,28 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var http = require('http');
 var jet = require('../lib/jet');
+
+var radarPort = parseInt(process.argv[3]) || 8080;
+var radarDir = process.argv[2] || '../radar/';
+
 jet.createDaemon().listen({
-    tcpPort: 11122
+    tcpPort: 11122,
+    wsPort: 11123
 });
+
+
+http.createServer(function (req, res) {
+  fs.readFile(radarDir + req.url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(radarPort);
+
+
