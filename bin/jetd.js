199fdf1e168f40baa2c7aac6e81dd4964dca1d51
Gerhard Lipp <gelipp@gmail.com>
2013-07-16 08:51:50 +0200

    redirect to radar.html, set correct content-type

--- bin/jetd.js
+++ bin/jetd.js
@@ -3,26 +3,34 @@
 var fs = require('fs');
 var http = require('http');
 var jet = require('../lib/jet');
+var path = require('path');
 
 var radarPort = parseInt(process.argv[3]) || 8080;
 var radarDir = process.argv[2] || '../radar/';
+
+var mime = {
+    '.js': 'application/javascript',
+    '.html': 'text/html',
+    '.css': 'text/css'
+}
 
 jet.createDaemon().listen({
     tcpPort: 11122,
     wsPort: 11123
 });
 
-
-http.createServer(function (req, res) {
-  fs.readFile(radarDir + req.url, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
+http.createServer(function(req, res) {
+    if (req.url === '/') {
+        req.url = '/radar.html';
     }
-    res.writeHead(200);
-    res.end(data);
-  });
+    fs.readFile(radarDir + req.url, function(err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.setHeader('Content-Type', mime[path.extname(req.url)]);
+        res.writeHead(200);
+        res.end(data);
+    });
 }).listen(radarPort);
-
-
