dbda243267e25e346c9e8dbc6566371c788620ff
Gerhard Lipp <gelipp@gmail.com>
2013-07-17 14:29:18 +0200

    improve async state interface

--- bin/some-service.js
+++ bin/some-service.js
@@ -5,7 +5,20 @@ var jet = require('../lib/jet');
 var peer = new jet.Peer();
 
 peer.state({
-    path: 'test',
+    path: 'acceptAll',
     value: 123,
-    set: function (val) {}
+    set: function (val) {
+        console.log('acceptAll is now', val);
+    }
+});
+
+peer.state({
+    path: 'acceptAllButSlow',
+    value: 123,
+    set: function (val, done) {
+        setTimeout(function () {
+            console.log('acceptAllButSlow is now', val);
+            done();
+        }, 1000);
+    }
 });
