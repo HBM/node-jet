3944bc715850902f9b1c3b8b6715ba817728b032
Gerhard Lipp <gelipp@gmail.com>
2013-08-08 14:31:21 +0200

    add some person states

--- bin/some-service.js
+++ bin/some-service.js
@@ -22,3 +22,39 @@ peer.state({
         }, 1000);
     }
 });
+
+// generates a function which assigns the value passed in to
+// the variable target
+var assign = function (target) {
+    return function (val) {
+        target = val;
+    }
+};
+
+var persons = [];
+
+var firstNames = ['Paul', 'Ben', 'George', 'Steve', 'Bill', 'Bert', 'Jimmy', 'Hank', 'Jessy'];
+var lastNames = ['Schroeder', 'Irish', 'Foreman', 'Mandy', 'Jops', 'Black', 'White', 'Lee'];
+var randomEntry = function (array) {
+    var index = Math.random() * array.length;
+    index = Math.floor(index);
+    return array[index];
+};
+
+for (var i = 0; i < 100; ++i) {
+    persons.push({
+        age: Math.floor(Math.random() * 100),
+        firstName: randomEntry(firstNames),
+        lastName: randomEntry(lastNames),
+        score: Math.floor(Math.random() * 100000)
+    });
+}
+
+
+persons.forEach(function (person, index) {
+    peer.state({
+        path: 'persons/' + index,
+        value: person,
+        set: assign(person)
+    });
+});
