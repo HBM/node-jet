e3a4340762965a55736b8f9e4fda1826cc7ad25e
Gerhard Preuss <gelipp@gmail.com>
2014-12-30 23:08:43 +0100

    beautify todo app

--- bin/some-service.js
+++ bin/some-service.js
@@ -111,37 +111,37 @@ var todos = {};
 var tid = 0;
 
 peer.method({
-  path: 'todo/add',
-  call: function(todo) {
-    var todoId = ++tid;
-    var todo = peer.state({
-      path: 'todo/#' + todoId,
-      value: {
-        completed: todo.completed,
-        title: todo.title,
-        id: todoId
-      },
-      set: function(todo) {
-        return {
-          value: {
-            completed: todo.completed,
-            title: todo.title,
-            id: todo.id
-          }
-        };
-      }
-    });
+	path: 'todo/add',
+	call: function (todo) {
+		var todoId = ++tid;
+		var todo = peer.state({
+			path: 'todo/#' + todoId,
+			value: {
+				completed: todo.completed,
+				title: todo.title,
+				id: todoId
+			},
+			set: function (todo) {
+				return {
+					value: {
+						completed: todo.completed,
+						title: todo.title,
+						id: todo.id
+					}
+				};
+			}
+		});
 
-    todos[todoId] = todo;
-  }
+		todos[todoId] = todo;
+	}
 });
 
 peer.method({
-  path: 'todo/remove',
-  call: function() {
-    var args = Array.prototype.slice.call(arguments);
-    args.forEach(function (todo) {
-      todos[todo.id].remove();
-    });
-  }
+	path: 'todo/remove',
+	call: function () {
+		var args = Array.prototype.slice.call(arguments);
+		args.forEach(function (todo) {
+			todos[todo.id].remove();
+		});
+	}
 });
