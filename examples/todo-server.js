b895effc85d7b86bfaeabe9dad407aa2e5e717d8
Gerhard Preuss <gelipp@gmail.com>
2014-12-30 23:42:48 +0100

    fix run todo on nodejitsu

--- examples/todo-server.js
+++ examples/todo-server.js
@@ -1,43 +1,63 @@
 #!/usr/bin/env node
 var jet = require('../lib/jet');
+var express = require('express');
+var http = require('http');
 
-var peer = new jet.Peer({url:'ws://localhost:11123'});
+var app = express();
+app.use(express.static('./'));
+
+var server = http.createServer(app);
+server.listen(80);
+
+var daemon = new jet.Daemon();
+daemon.listen({
+  server: server,
+  tcpPort: 11123
+});
+
+var peer = new jet.Peer({
+  onOpen: function() {
+    console.log('todo-server peer connected to daemon');
+  },
+  name: 'todo-server',
+  port: 11123
+});
 
 var todos = {};
 var id = 0;
 
 peer.method({
-	path: 'todo/add',
-	call: function (todo) {
-		var todoId = ++id;
-		var todo = peer.state({
-			path: 'todo/#' + todoId,
-			value: {
-				completed: todo.completed,
-				title: todo.title,
-				id: todoId
-			},
-			set: function (todo) {
-				return {
-					value: {
-						completed: todo.completed,
-						title: todo.title,
-						id: todo.id
-					}
-				};
-			}
-		});
+  path: 'todo/add',
+  call: function(todo) {
+    var todoId = ++id;
+    var todo = peer.state({
+      path: 'todo/#' + todoId,
+      value: {
+        completed: todo.completed,
+        title: todo.title,
+        id: todoId
+      },
+      set: function(todo) {
+        return {
+          value: {
+            completed: todo.completed,
+            title: todo.title,
+            id: todo.id
+          }
+        };
+      }
+    });
 
-		todos[todoId] = todo;
-	}
+    todos[todoId] = todo;
+  }
 });
 
 peer.method({
-	path: 'todo/remove',
-	call: function () {
-		var args = Array.prototype.slice.call(arguments);
-		args.forEach(function (todo) {
-			todos[todo.id].remove();
-		});
-	}
+  path: 'todo/remove',
+  call: function() {
+    var args = Array.prototype.slice.call(arguments);
+    args.forEach(function (todo) {
+      todos[todo.id].remove();
+    });
+  }
 });
--- nodejitsu-demo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-./bin/jetdws.js &
-sleep 2
-./examples/todo-server.js
