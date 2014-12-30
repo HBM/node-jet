0ca4b018c5fe3099370bbee890691c9217699f2d
Gerhard Preuss <gelipp@gmail.com>
2014-12-30 21:53:12 +0100

    fix fetch all

--- bin/fetch.js
+++ bin/fetch.js
@@ -4,6 +4,6 @@ var jet = require('../lib/jet');
 
 var peer = new jet.Peer();
 
-peer.fetch('.*', function (path, event, value) {
+peer.fetch({}, function (path, event, value) {
 	console.log(path, event, value);
 });
