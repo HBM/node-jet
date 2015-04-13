7591080a7a0209b9d5113d1da68fce50d14e4717
Gerhard Preuss <gelipp@gmail.com>
2015-04-13 14:23:21 +0200

    use default ws port 11123

--- examples/browser/script.js
+++ examples/browser/script.js
@@ -1,8 +1,8 @@
-
 // init jet connection using browserified node-jet where 'jet' is a global.
 var peer = new jet.Peer({
-  url: 'ws://localhost:11122',
-  onOpen: function() {
-    console.log('connection to Daemon established');
-  }
+	url: 'ws://localhost:11123'
+});
+
+peer.on('open', function (info) {
+	console.log('connection to Daemon established', info);
 });
