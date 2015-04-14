c5557e949eb7bea38df64ce0ff97623f6990b524
Gerhard Preuss <gelipp@gmail.com>
2015-04-14 22:53:29 +0200

    add render method. beautify

--- examples/todo/todo-client.js
+++ examples/todo/todo-client.js
@@ -1,46 +1,59 @@
 var jet = require('node-jet');
 
-var peer = new jet.Peer({url: 'ws://' + window.location.host});
+var peer = new jet.Peer({
+	url: 'ws://' + window.location.host
+});
 
-var addTodo = function(title) {
-  peer.call('todo/add', [title]);
+var addTodo = function (title) {
+	peer.call('todo/add', [title]);
 };
 
-var removeTodo = function(id) {
-  peer.call('todo/remove', [id]);
+var removeTodo = function (id) {
+	peer.call('todo/remove', [id]);
 };
 
-var removeAllTodos = function() {
-  peer.call('todo/remove', []);
+var removeAllTodos = function () {
+	peer.call('todo/remove', []);
 };
 
-var editTodo = function(id, title, completed) {
-  peer.set('todo/#' + id, {title: title, completed: completed});
+var editTodo = function (id, title, completed) {
+	peer.set('todo/#' + id, {
+		title: title,
+		completed: completed
+	});
 };
 
 peer.fetch({
-  path: {
-  	startsWith: 'todo/#'
-  },
-  sort: {
-    byValueField: {
-	  id: 'number'
+	path: {
+		startsWith: 'todo/#'
 	},
-	from: 1,
-	to: 30,
-	asArray: true
-  }
-}, function(todos) {
-  renderTodos(todos);
+	sort: {
+		byValueField: {
+			id: 'number'
+		},
+		from: 1,
+		to: 30,
+		asArray: true
+	}
+}, function (todos) {
+	renderTodos(todos);
 });
 
-var renderTodos = function(todos) {
+var renderTodos = function (todos) {
+	var root = document.getElementById('todos');
+	while (root.firstChild) {
+		root.removeChild(root.firstChild);
+	}
+	todos.forEach(function (todo) {
+		var titleElement = document.createElement('h3');
+		titleElement.innerText = todo.value.title;
+		root.appendChild(titleElement);
+	});
 	console.log(todos);
 };
 
-var addButton = document.getElementById('add-button');
-
-addButton.addEventListener('click', function() {
-	var title = document.getElementById('title-input').value;
-	addTodo(title);
+document.getElementById('add-button').addEventListener('click', function () {
+	var titleInput = document.getElementById('title-input');
+	addTodo(titleInput.value);
+	titleInput.value = '';
 });
