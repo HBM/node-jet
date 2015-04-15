62409c40355a6cb6cd62c9b9709877481cbbb65d
Gerhard Preuss <gelipp@gmail.com>
2015-04-15 10:33:19 +0200

    ignore all node_modules paths

--- beautify.sh
+++ beautify.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-find ./lib ./bin ./examples ./test -name "*.js" | xargs node_modules/.bin/js-beautify -j -r -t --good-stuff
+find ./lib ./bin ./examples ./test -name "*.js" | grep -v node_modules | xargs node_modules/.bin/js-beautify -j -r -t --good-stuff
