b0cae7786cd68c8077e1d66f7490755769116ca9
Gerhard Preuss <gelipp@gmail.com>
2015-05-19 08:54:24 +0200

    add ticker example snippet

---
+++ examples/snippets/ticker.js
@@ -0,0 +1,13 @@
+var jet = require('../../lib/jet');
+
+var peer = new jet.Peer({
+	url: 'ws://localhost:11123'
+});
+
+var ticker = new jet.State('ticker', 0);
+
+peer.add(ticker).then(function () {
+	setInterval(function () {
+		ticker.value(ticker.value() + 1);
+	}, 300);
+});
