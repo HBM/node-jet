612fe3dd5a545d7b4ffb39e8d019aa47e13b3ac6
Gerhard Preuss <gelipp@gmail.com>
2015-06-01 11:48:18 +0200

    fix to.be.true is not a function

--- test/access-tests.js
+++ test/access-tests.js
@@ -4,23 +4,23 @@ var access = require('../lib/jet/daemon/access');
 describe('The jet.daemon.access module', function () {
 
 	it('intersects(["a","b"],["b","c"]) === true', function () {
-		expect(access.intersects(['a', 'b'], ['b', 'c'])).to.be.true();
+		expect(access.intersects(['a', 'b'], ['b', 'c'])).to.be.true;
 	});
 
 	it('intersects(["a"],["b","c","a"]) === true', function () {
-		expect(access.intersects(['a'], ['b', 'c', 'a'])).to.be.true();
+		expect(access.intersects(['a'], ['b', 'c', 'a'])).to.be.true;
 	});
 
 	it('intersects(["a","b","c"],["c"]) === true', function () {
-		expect(access.intersects(['a', 'b', 'c'], ['c'])).to.be.true();
+		expect(access.intersects(['a', 'b', 'c'], ['c'])).to.be.true;
 	});
 
 	it('intersects(["a","b"],["c","b"]) === true', function () {
-		expect(access.intersects(['a', 'b'], ['c', 'b'])).to.be.true();
+		expect(access.intersects(['a', 'b'], ['c', 'b'])).to.be.true;
 	});
 
 	it('intersects(["a","b"],["e","c"]) === false', function () {
-		expect(access.intersects(['a', 'b'], ['e', 'c'])).to.be.false();
+		expect(access.intersects(['a', 'b'], ['e', 'c'])).to.be.false;
 	});
 
 });
