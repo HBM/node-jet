e717ed8ff511bac89f9e7f688d6a97c087fc978c
Gerhard Preuss <gelipp@gmail.com>
2015-06-08 13:33:54 +0200

    add http.get test for error urls

--- test/error-tests.js
+++ test/error-tests.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var errors = require('../lib/jet/errors');
+var https = require('https');
 
 describe('The jet.errors module', function () {
 	[
@@ -83,8 +84,10 @@ describe('The jet.errors module', function () {
 				expect(err.stack).to.be.a('string');
 			});
 
-			it('.url begins with http', function () {
-				expect(err.url.match(/^http/)).not.to.equal(null);
+			it('http.get(err.url) succeeds', function (done) {
+				https.get(err.url, function () {
+					done();
+				}).on('error', done);
 			});
 
 		});
