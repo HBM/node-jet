17aac09cb5ac0a590d18d6cc6e7c35f343da7536
Gerhard Preuss <gelipp@gmail.com>
2015-06-16 15:05:50 +0200

    improve ants example

--- examples/ants/ants-client.js
+++ examples/ants/ants-client.js
@@ -9,41 +9,70 @@ var peer = new jet.Peer({
 	url: 'ws://' + window.location.host
 });
 
-var renderAnts = function (ants) {
-	var circles = svgContainer.selectAll('circle');
-	circles
-		.data(ants)
-		.enter()
-		.append('circle')
-		.attr('r', 4)
-		.attr('cx', function (ant) {
-			return ant.value.x;
-		})
-		.attr('cy', function (ant) {
-			return ant.value.y;
-		});
+var ants = {};
 
-	circles
-		.transition()
-		.attr('cx', function (ant) {
-			return ant.value.x;
-		})
-		.attr('cy', function (ant) {
-			return ant.value.y;
-		});
-
+var renderAnt = function (ant) {
+	var circle;
+	if (ant.event === 'add') {
+		ants[ant.path] = circle = svgContainer.append('circle');
+		circle
+			.transition()
+			.attr('r', 4)
+			.attr('cx', ant.value.pos.x)
+			.attr('cy', ant.value.pos.y)
+			.style('fill', ant.value.color);
+	} else if (ant.event === 'change') {
+		circle = ants[ant.path];
+		circle
+			.transition()
+			.attr('cx', ant.value.pos.x)
+			.attr('cy', ant.value.pos.y)
+			.style('fill', ant.value.color);
+	} else {
+		circle = ants[ant.path];
+		circle
+			.remove();
+		delete ants[ant.path];
+	}
 };
 
-var ants = new jet.Fetcher()
+var allAnts = new jet.Fetcher()
 	.path('startsWith', 'ants/#')
-	.sortByPath()
-	.range(1, 100)
-	.on('data', function (ants) {
-		renderAnts(ants)
+	.on('data', function (ant) {
+		renderAnt(ant)
 	});
 
-peer.fetch(ants);
+peer.fetch(allAnts);
 
-var svgContainer = d3.select('body').append('svg')
-	.attr('width', '100%')
-	.attr('height', '100%');
+var delayValue = new jet.Fetcher()
+	.path('equals', 'ants/delay')
+	.on('data', function (data) {
+		d3.select('#delay').attr('value', data.value);
+	});
+
+peer.fetch(delayValue);
+
+d3.select('#delay')
+	.on('change', function () {
+		var val = d3.select('#delay').attr('value');
+		peer.set('ants/delay', event.target.value);
+	});
+
+var svgContainer = d3.select('svg')
+	.attr('width', 200 + 2)
+	.attr('height', 200 + 2);
+
+d3.select('#shake')
+	.on('click', function () {
+		peer.call('ants/shake', []);
+	});
+
+d3.select('#add')
+	.on('click', function () {
+		peer.call('ants/add');
+	});
+
+d3.select('#remove')
+	.on('click', function () {
+		peer.call('ants/remove');
+	});
--- examples/ants/ants-server.js
+++ examples/ants/ants-server.js
@@ -31,26 +31,72 @@ var peer = new jet.Peer({
 	port: internalPort
 });
 
-[1, 2, 3].forEach(function (item) {
-	var ant = new jet.State('ants/#' + item, {
-		x: 10 + item * 10,
-		y: 20
+var randomPos = function () {
+	return {
+		x: Math.abs(Math.random() * 200),
+		y: Math.abs(Math.random() * 200)
+	};
+};
+
+var randomColor = function () {
+	var hue = Math.abs(Math.random()) * 360;
+	var saturation = 40 + Math.abs(Math.random()) * 60;
+	return 'hsl(' + hue + ',' + saturation + '%,60%)';
+};
+
+var ants = [];
+
+var id = 0;
+
+var createAnt = function () {
+	var pos = randomPos();
+	var color = randomColor();
+	var ant = new jet.State('ants/#' + id++, {
+		pos: pos,
+		color: color
 	});
-	peer.add(ant);
-	setInterval(function () {
-		var lastX = ant.value().x;
-		var lastY = ant.value().y;
-		lastY = Math.abs(Math.random()) * 200;
-		ant.value({
-			x: lastX,
-			y: lastY
-		});
-	}, 300 + Math.abs(Math.random()) * 600);
+	peer.add(ant).then(function () {
+		ants.push(ant);
+	});
+};
+
+[1, 2, 3].forEach(createAnt);
+
+var delay = new jet.State('ants/delay', 100);
+delay.on('set', jet.State.acceptAny);
+
+var shake = new jet.Method('ants/shake');
+shake.on('call', function (args) {
+	ants.forEach(function (ant, index) {
+		setTimeout(function () {
+			var pos = randomPos();
+			var color = randomColor();
+			ant.value({
+				pos: pos,
+				color: color
+			});
+		}, index * delay.value());
+	});
+});
+
+var add = new jet.Method('ants/add');
+add.on('call', function (args) {
+	createAnt();
+});
+
+var remove = new jet.Method('ants/remove');
+remove.on('call', function (args) {
+	var last = ants.pop();
+	last.remove();
 });
 
 // connect peer and register methods
 jet.Promise.all([
 	peer.connect(),
+	peer.add(shake),
+	peer.add(delay),
+	peer.add(add),
+	peer.add(remove)
 ]).then(function () {
 	console.log('ants-server ready');
 	console.log('listening on port', port);
