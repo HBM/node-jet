b27899dc3bcb3fab62f92c364d72c310f2dac494
Administrator <florian.schopp@hbkworld.com>
2022-11-14 10:01:17 +0100

    fixed log test

--- test/log.test.ts
+++ test/log.test.ts
@@ -1,11 +1,16 @@
 import { Logger, LogLevel } from '../src/3_jet/log'
-import { readFileSync, mkdirSync, rmSync } from 'fs'
+import { readFileSync, mkdirSync, rmSync, existsSync } from 'fs'
 describe('Testing Logging', () => {
   beforeAll(() => {
+    if (existsSync('tmp')) {
+      rmSync('tmp', { recursive: true })
+    }
     mkdirSync('tmp')
   })
   afterAll(() => {
-    rmSync('tmp', { recursive: true })
+    if (existsSync('tmp')) {
+      rmSync('tmp', { recursive: true })
+    }
   })
   const log = (logger: Logger) => {
     logger.log('Bar', LogLevel.socket)
@@ -22,11 +27,11 @@ describe('Testing Logging', () => {
     return logger.flush()
   }
   describe('Should test callbacks', () => {
-    it('Schould create empty logger', () => {
+    it('Should create empty logger', () => {
       const logger = new Logger()
       log(logger)
     })
-    it('Schould create sock logger', () => {
+    it('Should create sock logger', () => {
       const logSpy = jest.fn()
       const logger = new Logger({
         loglevel: LogLevel.socket,
@@ -36,7 +41,7 @@ describe('Testing Logging', () => {
       log(logger)
       expect(logSpy).toHaveBeenCalledTimes(11)
     })
-    it('Schould create debug logger', () => {
+    it('Should create debug logger', () => {
       const logSpy = jest.fn()
       const logger = new Logger({
         loglevel: LogLevel.debug,
@@ -46,7 +51,7 @@ describe('Testing Logging', () => {
       log(logger)
       expect(logSpy).toHaveBeenCalledTimes(9)
     })
-    it('Schould create info logger', () => {
+    it('Should create info logger', () => {
       const logSpy = jest.fn()
       const logger = new Logger({
         loglevel: LogLevel.info,
@@ -56,7 +61,7 @@ describe('Testing Logging', () => {
       log(logger)
       expect(logSpy).toHaveBeenCalledTimes(6)
     })
-    it('Schould create warn logger', () => {
+    it('Should create warn logger', () => {
       const logSpy = jest.fn()
       const logger = new Logger({
         loglevel: LogLevel.warn,
@@ -66,7 +71,7 @@ describe('Testing Logging', () => {
       log(logger)
       expect(logSpy).toHaveBeenCalledTimes(4)
     })
-    it('Schould create error logger', () => {
+    it('Should create error logger', () => {
       const logSpy = jest.fn()
       const logger = new Logger({
         loglevel: LogLevel.error,
@@ -78,73 +83,64 @@ describe('Testing Logging', () => {
     })
   })
   describe('Should test file', () => {
-    it('Schould create empty logger', () => {
-      const logger = new Logger()
-      log(logger)
-    })
-    it('Schould create sock logger', (done) => {
+    it('Should create sock logger', async () => {
       const logger = new Logger({
         loglevel: LogLevel.socket,
         logname: 'Foo',
         logfile: 'tmp/sock.txt'
       })
-      log(logger).then(() => {
+      await log(logger).then(() => {
         const content = readFileSync('tmp/sock.txt').toString().split('\n')
         expect(content).toHaveLength(12)
         expect(content[0]).toContain('Foo	socket	Bar')
-        done()
       })
     })
-    it('Schould create debug logger', (done) => {
+    it('Should create debug logger', async () => {
       const logger = new Logger({
         loglevel: LogLevel.debug,
         logname: 'Foo',
         logfile: 'tmp/debug.txt'
       })
-      log(logger).then(() => {
+      await log(logger).then(() => {
         const content = readFileSync('tmp/debug.txt').toString().split('\n')
         expect(content).toHaveLength(10)
         expect(content[0]).toContain('Foo	debug	Bar')
-        done()
       })
     })
-    it('Schould create info logger', (done) => {
+    it('Should create info logger', async () => {
       const logger = new Logger({
         loglevel: LogLevel.info,
         logname: 'Foo',
         logfile: 'tmp/info.txt'
       })
-      log(logger).then(() => {
+      await log(logger).then(() => {
         const content = readFileSync('tmp/info.txt').toString().split('\n')
         expect(content).toHaveLength(7)
         expect(content[0]).toContain('Foo	info	Bar')
-        done()
       })
     })
-    it('Schould create warn logger', (done) => {
+    it('Should create warn logger', async () => {
       const logger = new Logger({
         loglevel: LogLevel.warn,
         logname: 'Foo',
         logfile: 'tmp/warn.txt'
       })
-      log(logger).then(() => {
+      await log(logger).then(() => {
         const content = readFileSync('tmp/warn.txt').toString().split('\n')
         expect(content).toHaveLength(5)
         expect(content[0]).toContain('Foo	warn	Bar')
-        done()
       })
     })
-    it('Schould create error logger', (done) => {
+    it('Should create error logger', async () => {
       const logger = new Logger({
         loglevel: LogLevel.error,
         logname: 'Foo',
         logfile: 'tmp/error.txt'
       })
-      log(logger).then(() => {
+      await log(logger).then(() => {
         const content = readFileSync('tmp/error.txt').toString().split('\n')
         expect(content).toHaveLength(3)
         expect(content[0]).toContain('Foo	error	Bar')
-        done()
       })
     })
   })
